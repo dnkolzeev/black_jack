b196fa08eb9ec6a5f7e99e3beebcc3b6d8f4bb4b
dnkolzeev <76228753+dnkolzeev@users.noreply.github.com>
2024-09-02 16:57:35 +0300

    templates

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+@app.route('/submit', methods=['GET', 'POST'])
+def handle_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return redirect(f"/thank_you?name={name}")
+    return render_template('form.html')
+
+@app.route('/thank_you')
+def thank_you():
+    name = request.args.get('name')
+    return f"Thank you, {name}, for submitting the form!"
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run()
