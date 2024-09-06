f3ffc65c02adf3ffc565a183ec5efdb5e247d826
dnkolzeev <76228753+dnkolzeev@users.noreply.github.com>
2024-09-06 11:24:08 +0300

    counter

--- app.py
+++ app.py
@@ -1,30 +1,51 @@
+#Импорт нужных библиотек
 from flask import Flask, render_template
 import json
 import requests
 
+
 app = Flask(__name__)
 
+#Функция для конвертации номинала в очки
+def converter(value):
+  if value == 'KING' or value == 'JACK' or value == 'QUEEN' or value == 'ACE':
+    points = 10
+  else:
+    points = int(value)
+  return points
+
+
+#Страничка с началом игры
 @app.route('/')
 def start():
-	return render_template('index.html')
+  return render_template('index.html')
 
+#Загружаем колоду и создаем стол игрока
 deck_id= json.loads(requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['deck_id']
 cards=[]
+points=[]
 
+
+#Даем игроку колоду и предлагаем взять первую карту
 @app.route('/game_start')
 def id(): 
   return render_template('game_start.html', deck_id = deck_id)
 
+#Стол игрока
 @app.route('/draw')
 def get_deck():
 	
-	deck_id= json.loads(requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['deck_id']
-	card=json.loads(requests.post('https://deckofcardsapi.com/api/deck/' +deck_id +'/draw/?count=1').text)['cards'][0]
-	card_image = card ['image']
-	card_value = card ['value']
-	card_code = card ['code']
-	cards.append(card_image)
-	return render_template('draw.html',card=card, card_image=card_image, card_value=card_value, card_code=card_code, cards=cards)
+  deck_id= json.loads(requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['deck_id']
+  card=json.loads(requests.post('https://deckofcardsapi.com/api/deck/' +deck_id +'/draw/?count=1').text)['cards'][0]
+  card_image = card ['image']
+  card_value = card ['value']
+  card_code = card ['code']
+  cards.append(card_image)
+  card_point = converter(card_value)
+  points.append(card_point)
+  points_sum = sum(points)
+  
+  return render_template('draw.html',card=card, card_image=card_image, card_value=card_value, card_code=card_code, cards=cards, card_point = card_point, points_sum = points_sum)
 
 if __name__ == '__main__':
   app.run(debug=True)
